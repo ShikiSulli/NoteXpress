197cee6ce397abf89c1f5dcf29e0aec6533f7066
ShikiSulli <benjaminbandasavanh@gmail.com>
2023-09-28 18:28:13 +0200

    ajout des fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use App\Entity\Category;
+use App\Entity\Note;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +13,55 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create($fakerLocale = 'fr_FR');
+        
+        $objectUser = [];
+        // Les user
+        for ($i = 0; $i < 100; $i++) {
+            $user = new User();
+            $user->setUsername($faker->userName())
+                ->setPassword($faker->password())
+                ->setRoles([]);
+            // On ajout l'objet user dans le tableau
+            array_push($objectUser, $user);
+            // On persiste l'objet user
+            $manager->persist($user);
+        }
 
+        $categories = [
+            'Travail',
+            'Bricolage',
+            'Achats',
+            'Administratif',
+            'Formation',
+        ];
+
+        $objectCategory = [];
+
+        for ($i = 0; $i < count($categories); $i++) {
+            $category = new Category;
+            $category->setName($categories[$i])
+            ->setUser($objectUser[rand(0,99)]);
+
+            array_push($objectCategory, $category);
+          
+            $manager->persist($category);
+        }
+       
+
+        $objectNote = [];
+        // Les Notes
+        for ($i = 0; $i < 100; $i++) {
+            $note = new Note();
+            $note->setTitle($faker->sentence(2))
+                ->setContent($faker->sentence(5))
+                ->setCategory($objectCategory[rand(0,4)])
+                ->setUser($objectUser[rand(0,99)]);
+            
+            array_push($objectNote, $note);
+            
+            $manager->persist($note);
+        }
         $manager->flush();
     }
 }
